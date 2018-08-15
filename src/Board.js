fd07d900783f93b1d4f91f1cc044812f92cb30d6
iprincewang <>
2018-08-15 20:43:38 +0800

    添加展示逻辑

--- src/Board.js
+++ src/Board.js
@@ -2,13 +2,66 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Square from "./Square";
 
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
 export default class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squars: Array(9).fill(null),
+      xIsNext:true,
+    };
+  }
+
+  handleClick(i){
+    const squars = this.state.squars.slice();//slice()方法为浅拷贝
+    if(calculateWinner(squars) || squars[i]){
+        return;
+    }
+    squars[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+        squars : squars,
+        xIsNext : !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+        <Square 
+            value = {this.state.squars[i]} 
+            onClick = {()=>this.handleClick(i)}
+        />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squars);
+    let status;
+    if(winner){
+        status = 'Winner: ' + winner;
+    }
+    else{
+        status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className>
         <div>
--- src/Square.js
+++ src/Square.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default class Square extends React.Component {
+
+    constructor(){
+        super();
+    }
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick = {() => this.props.onClick()}>
                 {this.props.value}
             </button>
         );
